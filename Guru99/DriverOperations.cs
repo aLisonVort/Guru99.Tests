53aa708f3d0a7d1fb3132dc88470176bebe86020
aLisonVort <60932858+aLisonVort@users.noreply.github.com>
2020-04-13 18:41:18 +0300

    Add all files to repository

---
+++ Guru99/DriverOperations.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using System;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace Guru99
+{
+    class DriverOperations
+    {
+        public IWebDriver _driver { get; set; }
+        public IWebDriver OneTimeInitialize()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+            return GetDriverInstance();
+            
+        }
+        
+        public void CleanUp()
+        {
+            string testStatus = GetTestStatus();
+            string testName = GetTestName();
+            Console.WriteLine($"Test status of {testName} is {testStatus}");
+            Console.WriteLine("-------------"); 
+        }
+        public void KillDriverInstance()
+        {
+            _driver.Quit();
+        }
+        public string GetTestName()
+        {
+            return TestContext.CurrentContext.Test.Name;
+        }
+
+        public string GetTestStatus()
+        {
+            return TestContext.CurrentContext.Result.Outcome.Status.ToString();
+        }
+        public IWebDriver GetDriverInstance()
+        {
+            return _driver;
+        }
+    }
+}
---
+++ Guru99/Interfaces/IPages.cs
@@ -0,0 +1,15 @@
+﻿using Guru99.Pages;
+using Guru99.TestsInfo;
+using OpenQA.Selenium;
+
+namespace Guru99.Interfaces
+{
+    public interface IPages
+    {
+        IPages GetAttributeValue(IWebElement e, string attribute);
+        IPages FixFormat(string actual, string desired);
+        IPages ElementIsSelected(IWebElement e);
+        IPages True();
+        void SetLoggerInstance(Logger logger);
+    }
+}
